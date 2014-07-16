6f249d7435d4d6c666a620aa1907a21c7c90502d
Firemark <marpiechula@gmail.com>
2014-07-16 21:44:28 +0200

    Return to one canvas node and detect size of canvas.

--- turing.js
+++ turing.js
@@ -23,8 +23,8 @@ function Machine() {
 		var coll = [];
 		for(i=0; i < numStates; i++){
 			var state = new State(
-				randomInt(-3, 3),
-				randomInt(-3, 3), 
+				randomInt(-1, 1),
+				randomInt(-1, 1), 
 				randomIndex(numStates),
 				randomIndex(numSymbols)
 			);
@@ -52,24 +52,30 @@ function Machine() {
 }
 
 function Tape(htmlId) {
+	var element = document.getElementById(htmlId);
 
-	this.ctx = document.getElementById(htmlId).getContext('2d');
-	this.symbolData = Array(256 * 256);
-	this.cursor = [127, 127];		
+	this.ctx = element.getContext('2d');
+	this.size = [element.width, element.height];
+	this.sizeSubOne = [element.width - 1, element.height - 1];
+	this.arr_size = element.width * element.height;
+
+	this.symbolData = Array(this.arr_size);
+	this.cursor = [Math.floor(element.width / 2),
+				   Math.floor(element.height / 2)];		
 
 	this.reset = function(symbolId){
 		symbolId = symbolId || 0;
-		for(i=0; i < 256 * 256; i++){
+		for(i=0; i < this.arr_size; i++){
 			this.symbolData[i] = symbolId;
 		}
 		this.ctx.fillStyle = 'rgb(' + symbols[symbolId] + ')';
-		this.ctx.fillRect(0, 0, 255, 255);
-		this.imageData = this.ctx.getImageData(0, 0, 255, 255);
+		this.ctx.fillRect(0, 0, this.sizeSubOne[0], this.sizeSubOne[1]);
+		this.imageData = this.ctx.getImageData(0, 0, this.sizeSubOne[0], this.sizeSubOne[1]);
 	}
 
 	this.random = function(){
 		var numSymbols = symbols.length;
-		for(i=0; i < 256 * 256; i++){
+		for(i=0; i < this.arr_size; i++){
 			this.setSymbol(randomIndex(numSymbols), i);
 		}
 		this.refresh();
@@ -79,15 +85,15 @@ function Tape(htmlId) {
 		for(var i in dir){
 			this.cursor[i] += dir[i];
 			var cur = this.cursor[i];
-			if (cur > 255)
-				this.cursor[i] -= 255;
+			if (cur > this.sizeSubOne[i])
+				this.cursor[i] -= this.sizeSubOne[i];
 			if (cur < 0)
-				this.cursor[i] += 255;
+				this.cursor[i] += this.sizeSubOne[i];
 		}
 	}
 
 	this.getIndex = function(){
-		return this.cursor[0] + this.cursor[1] * 256;
+		return this.cursor[0] + this.cursor[1] * this.size[0];
 	}
 
 	this.getSymbol = function(){
@@ -115,31 +121,31 @@ function Tape(htmlId) {
 	this.reset();
 }
 
-var tapes = [];
+var tape = null;
 var machines = [];
 
 function init(){
 
-	tapes = [new Tape('firstCanvas'), new Tape('secondCanvas')];
+	tape = new Tape('mainCanvas');
 	machines = [new Machine(), new Machine()];
-	//tapes[0].random();
+	//tape.random();
 	//tapes[1].random();
 	
 	window.setTimeout(change_state, 1);
 }
 
 function change_state(){
-	for(var i=0; i < 2; i++){
-		var tape = tapes[i];
-		var machine = machines[i];
+	for(var c=0; c < 32; c++){
+		var machine = machines[0];
 			
 		machine.changeState(tape.getSymbol());
 		var state = machine.getState();
 
 		tape.setSymbol(state.symbol);
 		tape.move(state.move);
-		tape.refresh();		
 	}
+	tape.refresh();
+
 
 	window.setTimeout(change_state, 1);
 }
