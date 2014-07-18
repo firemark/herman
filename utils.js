091b4e462d55f6b4c45ad180e78da9fd91b3bd30
Firemark <marpiechula@gmail.com>
2014-07-18 23:43:07 +0200

    add new util functions

--- utils.js
+++ utils.js
@@ -5,3 +5,23 @@ function randomInt(a, b) {
 function randomIndex(a) {
   return Math.floor((Math.random() * a));
 };
+
+function genArray(many, func){
+    var arr = [];
+    for(var i=0; i < many; i++)
+        arr.push(func(i));
+    return arr;
+
+}
+
+function addRow(args, node){
+    node = node || 'td';
+    var template = '<tr>';
+
+    var len = args.length;
+    for(var i=0; i < len; i++)
+        template += '<' + node + '>' + args[i] + '</' + node + '>';
+
+    template += '</tr>';
+    return template;
+}
